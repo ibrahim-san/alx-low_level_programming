428b60758ff0d739f0930768ffee6fb486b85134
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2023-02-16 23:49:57 +0100

    a quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - The execution entry point of the program
+ * Return: Integer 0 to indicate the end of the program
+ */
+int main(void)
+{
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, sizeof(message) - 1);
+return (1);
+}
