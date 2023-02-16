23572a9e72a6a1819dc52dc005a27ef5bf6bdbe0
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2023-02-16 23:01:48 +0100

    print using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ * main - The execution entry point of the program
+ * Return: Integer 0 to indicate the end of the program
+ */
 int main(void)
 {
-    char Mystr[] = "\"Programming is like building a multilingual puzzle";
-    puts(Mystr);
-    return (0);
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+puts(Mystr);
+return (0);
 }
