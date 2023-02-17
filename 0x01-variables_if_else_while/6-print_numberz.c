8afe0f71164d051764fc577c3d64dbdd238a2585
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2023-02-17 22:55:10 +0100

    print single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry point to the program
+ *Return: integer 0
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
