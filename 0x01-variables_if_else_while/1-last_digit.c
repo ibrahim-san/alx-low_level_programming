a9f1bfb8f19c3396aadfb20a951136a0bc566d05
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2023-02-17 20:18:22 +0100

    last digit of an integer

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 int last = n % 10;
 if (last > 5)
 {
-printf("is %d and is greater than 5\n", last);
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
 else if (last == 0)
 {
-printf("is %d and is 0\n", last);
+printf("Last digit of %d is %d and is 0\n", n, last);
 }
 else if (last < 6 and last != 0)
 {
-printf("is %d and is less than 6 and not 0\n", last);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
 return (0);
 }
