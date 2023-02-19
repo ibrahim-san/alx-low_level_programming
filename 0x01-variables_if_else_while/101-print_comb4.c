77a4ec56150ab4cd08feddb0729338eeaffd541f
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2023-02-19 21:06:53 +0100

    3 digit combos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: integer 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+int j;
+for (j = i + 1; j < 10; j++)
+{
+int k;
+for (k = j + 1; k < 10; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 8 || j != 9 || k != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
