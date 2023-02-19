6b5c6ad49dc88e67209050f0f40e195ab85ec079
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2023-02-19 20:06:31 +0100

    combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,11 @@ int main(void)
 int i;
 for (i = 0; i < 10; i++)
 {
-int j;
-for (j = 0; j < 10; j++)
-{
 putchar(i + '0');
-putchar(j + '0');
-if (i != 9 || j != 9)
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 return (0);
