16f70b2a01a1f1b5eb213d58e321d421ab561d83
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2023-02-20 21:55:57 +0100

    double double digit combos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: integer 0
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 99; i++)
+{
+for (j = i; j <= 99; j++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 99 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
